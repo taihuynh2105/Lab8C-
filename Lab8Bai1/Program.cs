9e33a212d8759e3413c60bae0b0e3273a327067a
taihuynh2105 <huynhletai210503@gmail.com>
2024-06-12 14:25:39 +0700

    Up landau

---
+++ Lab8Bai1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static Random random = new Random();
+    static object lockObj = new object();
+    static int randomNumber;
+    static bool complete = false;
+    static void Thread1()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            lock (lockObj)
+            {
+                randomNumber = random.Next(1, 11);
+                Console.WriteLine($"Thread1: Sinh so ngau nhien:{randomNumber}");
+                Monitor.Pulse(lockObj);
+                Monitor.Wait(lockObj);
+            }
+            Thread.Sleep(2000);
+        }
+        complete = true;
+    }
+    static void Thread2()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            lock (lockObj)
+            {
+                Monitor.Wait(lockObj);
+                int squaredNumber = randomNumber * randomNumber;
+                Console.WriteLine($"Thread2: Binh phuong cua so:{squaredNumber}");
+                Monitor.Pulse(lockObj);
+            }
+            Thread.Sleep(1000);
+        }
+    }
+    static void Main()
+    {
+        Thread thread1 = new Thread(Thread1);
+        Thread thread2 = new Thread(Thread2);
+        thread1.Start();
+        thread2.Start();
+
+        while (!complete)
+        {
+            Thread.Sleep(100);
+        }
+        Console.WriteLine("Ket thuc chuong trinh");
+    }
+}
+
+    
---
+++ Lab8Bai2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading;
+
+public class DeadlockExample
+{
+    static object lock1 = new object();
+    static object lock2 = new object();
+
+    static void Thread1FunctionLab8()
+    {
+        bool lock1Acquired = false;
+        bool lock2Acquired = false;
+        try
+        {
+            Monitor.TryEnter(lock1, TimeSpan.FromMilliseconds(500), ref lock1Acquired);
+            if (lock1Acquired)
+            {
+                Console.WriteLine("Thread 1 locked lock1");
+                Thread.Sleep(100); // Giả lập xử lý công việc
+                Console.WriteLine("Thread 1 is waiting for lock2");
+
+                Monitor.TryEnter(lock2, TimeSpan.FromMilliseconds(500), ref lock2Acquired);
+                if (lock2Acquired)
+                {
+                    Console.WriteLine("Thread 1 locked lock2");
+                }
+                else
+                {
+                    Console.WriteLine("Thread 1 could not lock lock2");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Thread 1 could not lock lock1");
+            }
+        }
+        finally
+        {
+            if (lock2Acquired)
+            {
+                Monitor.Exit(lock2);
+            }
+            if (lock1Acquired)
+            {
+                Monitor.Exit(lock1);
+            }
+        }
+    }
+
+    static void Thread2FunctionLab8()
+    {
+        bool lock1Acquired = false;
+        bool lock2Acquired = false;
+        try
+        {
+            Monitor.TryEnter(lock2, TimeSpan.FromMilliseconds(500), ref lock2Acquired);
+            if (lock2Acquired)
+            {
+                Console.WriteLine("Thread 2 locked lock2");
+                Thread.Sleep(100); // Giả lập xử lý công việc
+                Console.WriteLine("Thread 2 is waiting for lock1");
+
+                Monitor.TryEnter(lock1, TimeSpan.FromMilliseconds(500), ref lock1Acquired);
+                if (lock1Acquired)
+                {
+                    Console.WriteLine("Thread 2 locked lock1");
+                }
+                else
+                {
+                    Console.WriteLine("Thread 2 could not lock lock1");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Thread 2 could not lock lock2");
+            }
+        }
+        finally
+        {
+            if (lock1Acquired)
+            {
+                Monitor.Exit(lock1);
+            }
+            if (lock2Acquired)
+            {
+                Monitor.Exit(lock2);
+            }
+        }
+    }
+
+    public static void Main()
+    {
+        Thread thread1 = new Thread(Thread1FunctionLab8);
+        Thread thread2 = new Thread(Thread2FunctionLab8);
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine("End of Main");
+    }
+}
